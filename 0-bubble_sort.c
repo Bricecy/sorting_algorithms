28e02fbac642a456dbd70bb47da5d06b4b98ed65
Navashub <navasmuller01@gmail.com>
2023-04-25 12:36:51 +0300

    update on bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,31 +11,27 @@
  *
  * Return: void
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	int tmp;
+	int temp;
 	size_t i, j;
-	bool swapped;
 
-	if (!array)
+	if (array == NULL)
 		return;
 
-	swapped = 1;
-	for (i = 0; i < (size - 1) && swapped; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
-		for (j = 0; j < (size - 1 - i); j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				/* swapping the two elements */
-				tmp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-
-				swapped = 1;
+				array[j + 1] = array[j];
 				print_array(array, size);
 			}
 		}
 	}
 }
+
